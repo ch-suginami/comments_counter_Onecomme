a913a59f49dc74ed9f5b937480da38e19d059a4d
ch-suginami <67103794+ch-suginami@users.noreply.github.com>
2025-09-04 00:53:25 +0900

    バッチファイルと説明書の改訂

--- start.bat
+++ start.bat
@@ -5,7 +5,11 @@ if not "%~0"=="%~dp0.\%~nx0" (
      exit
 )
 
+set EXE_ONE_COMME=%LOCALAPPDATA%\Programs\OneComme\OneComme.exe
+echo %EXE_ONE_COMME%
+start %EXE_ONE_COMME%
+
 cd /d %~dp0
 set WD=%~dp0
 start python %WD%\comment_counter_ws_server.py
-start python %WD%\commnet_counter_ws.py
+start python %WD%\comment_counter_ws.py
